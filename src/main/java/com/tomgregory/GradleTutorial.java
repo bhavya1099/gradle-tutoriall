1de167987fee7d953e8a02a8c605f8f3cac9aae7
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-04-29 11:14:41 +0530

    push additional functions

--- src/main/java/com/tomgregory/GradleTutorial.java
+++ src/main/java/com/tomgregory/GradleTutorial.java
@@ -3,11 +3,18 @@ package com.tomgregory;
 public class GradleTutorial {
     public static void main(String[] args) {
         System.out.println("Gradle 4tw!");
+        int sum = addTwoNumbers(4,5);
+        int multiply = multiplyTwoNumbers(9,7);
+        System.out.println(sum);
+        System.out.println(multiply);
     }
-    public int addTwoNumbers(int a,int b){
+
+
+    public static int addTwoNumbers(int a,int b){
         return a+b;
     }
-    public int multiplyTwoNumbers(int a,int b){
+
+    public static int multiplyTwoNumbers(int a,int b){
         return a*b;
     }
 }
